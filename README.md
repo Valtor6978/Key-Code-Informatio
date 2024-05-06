96cd5c0b668844627331e436caabcebbc26e533f
Valtor <asfm.ggbot@gmail.com>
2024-05-06 20:57:27 +0430

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Receive key codes by pressing them
+
+
+
+
+
+
+### If you are a developer and need your keyboard information, you can get help from the following codes
+- [x] Key Sign 
+- [x] Key Location
+- [x] ASCII 
+- [x] Key Code
---
+++ js/script.js
@@ -0,0 +1,24 @@
+﻿let $ = document
+
+let keyCode = $.getElementById("keyCode")
+let key = $.getElementById("key")
+let location1 = $.getElementById("location1")
+let which = $.getElementById("which")
+let code = $.getElementById("code")
+
+
+document.body.addEventListener('keydown', function (event) {
+	
+	starter.style.display = 'none'
+	heading.style.display = 'flex'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+
+	event.preventDefault()
+
+	keyCode.innerHTML = event.keyCode
+	key.innerHTML = event.key
+	location1.innerHTML = event.location
+	which.innerHTML = event.which
+	code.innerHTML = event.code
+})
